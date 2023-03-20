88f68ec754a21d89f1bea3d201c7c54b77264550
porteratzo <omar@aifi.com>
2023-03-19 23:03:25 -0600

    conda stuff

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='treetool',
+    version='1.0.0',    
+    description='Python package for tree detection, segmentation and extraction of DBH',
+    url='https://github.com/porteratzo/TreeTool',
+    author='Omar Montoya',
+    author_email='omar.alfonso.montoya@hotmail.com',
+    license='MIT License',
+    packages=['treetool'],
+    install_requires=[,                     
+                      ],
+    classifiers=[
+    ],
+)
