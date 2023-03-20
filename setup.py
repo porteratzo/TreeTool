fd6b320c542196834de0972c6c804c6eada910c2
porteratzo <omar@aifi.com>
2023-03-19 23:05:17 -0600

    changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='treetool',
     version='1.0.0',    
     description='Python package for tree detection, segmentation and extraction of DBH',
-    url='https://github.com/porteratzo/TreeTool',
+    url='https://github.com/porteratzo/TreeTool/tree/setup',
     author='Omar Montoya',
     author_email='omar.alfonso.montoya@hotmail.com',
     license='MIT License',
